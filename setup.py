5278c584681fa0d98cd7963e0bbccf60f27b8e07
Utkarsh Patel <imutkarshpatel@gmail.com>
2022-03-25 01:11:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@ import io
 import os
 
 # Package meta-data
-NAME = 'twint'
-DESCRIPTION = 'An advanced Twitter scraping & OSINT tool.'
-URL = 'https://github.com/twintproject/twint'
-EMAIL = 'codyzacharias@pm.me'
-AUTHOR = 'Cody Zacharias'
-REQUIRES_PYTHON = '>=3.6.0'
+NAME = 'defog'
+DESCRIPTION = 'Single Image Defogging by Multiscale Depth Fusion'
+URL = 'https://github.com/utkarsh512/defog/'
+EMAIL = 'imutkarshpatel@gmail.com'
+AUTHOR = 'Utkarsh Patel'
+REQUIRES_PYTHON = '>=3.7.0'
 VERSION = None
 
 # Packages required
 REQUIRED = [
-    'aiohttp', 'aiodns', 'beautifulsoup4', 'cchardet', 'dataclasses',
-    'elasticsearch', 'pysocks', 'pandas', 'aiohttp_socks',
-    'schedule', 'geopy', 'fake-useragent', 'googletransx'
+    'numpy', 'opencv-python'
 ]
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -42,24 +40,22 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=['twint', 'twint.storage'],
+    packages=['defog'],
     entry_points={
         'console_scripts': [
-            'twint = twint.cli:run_as_command',
+            'defog = defog.cli:run_as_command',
         ],
     },
     install_requires=REQUIRED,
-    dependency_links=[
-        'git+https://github.com/x0rzkov/py-googletrans#egg=googletrans'
-    ],
     license='MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
 )
