792e793f6a4ae92d174b7a5ac2c700a8979c08e1
Utkarsh Patel <imutkarshpatel@gmail.com>
2022-04-10 15:50:10 +0530

    bug fixes

--- defog/cli.py
+++ defog/cli.py
@@ -3,7 +3,6 @@ defog - single image defogging by multiscale depth fusion
 """
 
 import sys
-import os
 import argparse
 import cv2
 import numpy as np
@@ -19,17 +18,6 @@ def error(_error, message):
     """
     print("[-] {}: {}".format(_error, message))
     sys.exit(0)
-
-
-def normalize(v):
-    """
-    Normalize for saving as image file
-    """
-    v_max = np.max(v)
-    v = np.divide(v, v_max)
-    v *= 255
-    v = v.astype(int)
-    return v
 
 
 def options():
@@ -50,10 +38,19 @@ def options():
     return args
 
 
+def make_image(v):
+    """
+    Normalize for saving as image file
+    """
+    v_max = np.max(v)
+    v = np.divide(v, v_max)
+    v *= 255
+    v = v.astype(int)
+    return v
+
+
 def main():
-    """
-    main
-    """
+    """main"""
     args = options()
     print(f"[*] Reading image from {args.input}")
     ip_image = cv2.imread(args.input)
@@ -62,6 +59,7 @@ def main():
     print(f"[DONE] Input image shape = {ip_image.shape}.")
     print(f"[*] Initializing depth map...")
     depth_map = init.init_depth_map(ip_image, args.patches)
+    cv2.imwrite("depth_map_init.jpg", make_image(depth_map.copy()))
     print(f"[DONE] Depth map initialized.")
     print(f"[*] Optimizing depth map...")
     depth_map = optimizer.optimize(depth_map=depth_map,
@@ -70,35 +68,34 @@ def main():
                                    lambda_=args.lambda_,
                                    threshold=args.threshold,
                                    v_max=args.v_max)
+    cv2.imwrite("depth_map_final.jpg", make_image(depth_map.copy()))
     print(f"[DONE] Depth map optimized.")
-    depth_map = normalize(depth_map)
+    depth_map_discrete = make_image(depth_map.copy())
+    depth_map = np.exp(-depth_map)
     print(f"[*] Estimating airlight...")
     edge_image = airlight.get_edge_image(ip_image)
-    fog_region = airlight.fog_opaque_region(edge_image, depth_map)
+    fog_region = airlight.fog_opaque_region(edge_image, depth_map_discrete)
     atm_lum = airlight.atmospheric_luminance(ip_image, fog_region)
     atm_image = ip_image.copy()
     h, w, c = ip_image.shape
     for i in range(h):
         for j in range(w):
             for k in range(c):
-                atm_image[i, j, k] = (255 - depth_map[i, j]) * atm_lum[k]
+                atm_image[i, j, k] = (1 - depth_map[i, j]) * atm_lum[k]
     print(f"[DONE] Airlight estimation done.")
     print(f"[*] Writing output image...")
     op_image = ip_image - atm_image
-    for k in range(c):
-        op_image[:, :, k] = np.divide(op_image[:, :, k], depth_map)
-    op_image = normalize(op_image)
-    op_image = airlight.preprocess(op_image)
     cv2.imwrite(args.output, op_image)
     print(f"[DONE] Output image written at {args.output}.")
 
 
 def run_as_command():
     version = ".".join(str(v) for v in sys.version_info[:2])
-    if float(version) < 3.6:
-        print("[-] defog requires Python version 3.6+.")
+    if float(version) < 3.7:
+        print("[-] defog requires Python version 3.7+.")
         sys.exit(0)
     main()
+
 
 if __name__ == '__main__':
     main()
